3a8a19d4effc19a31267f3c004322ff609f001ea
Krllka <k.r.llaleks25@gmail.com>
2021-06-17 06:28:11 +0400

    fix Order price

--- back/src/main/java/back/DAO/Impl/OrderDAO.java
+++ back/src/main/java/back/DAO/Impl/OrderDAO.java
@@ -1,8 +1,11 @@
 package back.DAO.Impl;
 
 import back.DAO.DAOinterface;
+import back.Entities.Car;
+import back.Entities.Car_Spec;
 import back.Entities.Order;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.stereotype.Repository;
@@ -26,7 +29,12 @@ public class OrderDAO extends AbstractDAO<Order>
     public void add(Order film) {
         Session session = sessionFactory.getCurrentSession();
         film.setDate(new Date(System.currentTimeMillis()));
-        film.setPrice(film.getCar().getPrice());
+
+        session = super.sessionFactory.getCurrentSession();
+        Query<Car> query = session.createQuery("From Car where id = :currID");
+        query.setParameter("currID", film.getCar().getId());
+        film.setPrice(query.getSingleResult().getPrice());
+        System.out.println(film.getPrice());
         session.save(film);
     }
 
